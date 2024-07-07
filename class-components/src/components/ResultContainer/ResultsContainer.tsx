6e165cea1b55327f8c60be391d1555c8d5d54962
Aleksey <volgaopa@gmail.com>
2024-07-08 01:50:36 +0300

    refactor: prepare project files

---
+++ class-components/src/components/ResultContainer/ResultsContainer.tsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import './ResultContainer.scss';
+
+export class ResultsContainer extends Component {
+  render() {
+    return <></>;
+  }
+}
---
+++ class-components/src/components/SearchingInput/SearchingInput.tsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import './SearchingInput.scss';
+
+export class SearchingInput extends Component {
+  render() {
+    return <></>;
+  }
+}
