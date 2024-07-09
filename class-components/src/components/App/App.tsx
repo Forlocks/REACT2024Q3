d14c102ef53fd9ce0d53915509ded838b30454bf
Aleksey <volgaopa@gmail.com>
2024-07-09 13:37:56 +0300

    feat: implement error boundary

--- class-components/src/components/App/App.tsx
+++ class-components/src/components/App/App.tsx
@@ -2,14 +2,17 @@ import { Component } from 'react';
 import { ErrorButton } from '../ErrorButton/ErrorButton';
 import { Searching } from '../Searching/Searching';
 import './App.scss';
+import { ErrorBoundary } from '../ErrorBoundary/ErrorBoundary';
 
 export class App extends Component {
   render() {
     return (
-      <div className="app">
-        <ErrorButton />
-        <Searching />
-      </div>
+      <ErrorBoundary>
+        <div className="app">
+          <ErrorButton />
+          <Searching />
+        </div>
+      </ErrorBoundary>
     );
   }
 }
