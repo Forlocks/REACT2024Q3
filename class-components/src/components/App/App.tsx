44d2f08f3672707059bb4aa15df0a0ce0d023e04
Aleksey <volgaopa@gmail.com>
2024-07-09 12:05:45 +0300

    feat: implement working with last query

--- class-components/src/components/App/App.tsx
+++ class-components/src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
-import { ErrorButton } from './components/ErrorButton/ErrorButton';
-import { Searching } from './components/Searching/Searching';
+import { ErrorButton } from '../ErrorButton/ErrorButton';
+import { Searching } from '../Searching/Searching';
 import './App.scss';
 
 export class App extends Component {
--- class-components/src/components/Searching/Searching.tsx
+++ class-components/src/components/Searching/Searching.tsx
@@ -1,6 +1,7 @@
 import { Component, ChangeEvent, FormEvent } from 'react';
 import { Card } from '../Card/Card';
 import './Searching.scss';
+import loading from '../../assets/loading.png';
 
 export interface Person {
   name: string;
@@ -13,48 +14,86 @@ export interface Person {
 interface SearchingData {
   textInput: string;
   results: Person[];
+  loading: boolean;
 }
 
 export class Searching extends Component {
   state: SearchingData = {
     textInput: '',
     results: [],
+    loading: false,
+  };
+
+  componentDidMount = () => {
+    const lastRequest = localStorage.getItem('ForlocksAPI') || '';
+
+    this.setState(
+      {
+        textInput: lastRequest,
+        loading: true,
+      },
+      () => {
+        this.handleSubmit();
+      },
+    );
   };
 
   handleInputTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     this.setState({
-      textInput: event.target.value,
+      textInput: event.target.value.trim(),
     });
   };
 
-  handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  handleSubmit = async (event?: FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+    this.setState({ loading: true });
+    localStorage.setItem('ForlocksAPI', this.state.textInput);
 
-    const url: string = 'https://swapi.dev/api/people/?page=1&search=';
-    const query: string = this.state.textInput;
+    let url: string;
+    const request: string = this.state.textInput;
+
+    if (request === '') {
+      url = 'https://swapi.dev/api/people/?page=1';
+    } else {
+      url = 'https://swapi.dev/api/people/?page=1&search=';
+    }
 
     try {
-      const response = await fetch(url + query);
+      const response = await fetch(url + request);
       const data = await response.json();
       this.setState({
         results: data.results,
+        loading: false,
       });
     } catch (error) {
+      this.setState({ loading: false });
       console.log(error);
     }
   };
 
   render() {
-    const results = this.state.results.map((item) => (
-      <Card
-        key={item.name}
-        name={item.name}
-        gender={item.gender}
-        birth_year={item.birth_year}
-        height={item.height}
-        mass={item.mass}
-      />
-    ));
+    let content;
+
+    if (this.state.loading) {
+      content = (
+        <div className="loading-container">
+          <img className="loading" src={loading} />
+        </div>
+      );
+    } else if (this.state.results.length > 0) {
+      content = this.state.results.map((item) => (
+        <Card
+          key={item.name}
+          name={item.name}
+          gender={item.gender}
+          birth_year={item.birth_year}
+          height={item.height}
+          mass={item.mass}
+        />
+      ));
+    } else {
+      content = <div className="no-results">no results found</div>;
+    }
 
     return (
       <>
@@ -62,13 +101,14 @@ export class Searching extends Component {
           <input
             className="input"
             placeholder="luke skywalker"
+            value={this.state.textInput}
             onChange={this.handleInputTextChange}
           ></input>
           <button className="button" type="submit">
             search
           </button>
         </form>
-        <div className="result-container">{results}</div>
+        <div className="result-container">{content}</div>
       </>
     );
   }
